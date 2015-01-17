76e9bf72a0d6214b1e28d4619170eca978edc81c
Roshan Kulkarni <roshan.kulkarni@mindstix.com>
2015-01-17 14:05:15 +0530

    Additional validation checks

--- lab1_part1/lab1.js
+++ lab1_part1/lab1.js
@@ -1,13 +1,15 @@
 //
 // Read JSON File and Write to a Output Flat File
 //
+"use strict";
 
-// Dependent Modules
+// Dependencies
 var fs = require("fs");
+var util = require("util");
 
 // Configuration
-var inputFileName = "source.json";
-var outputFileName = "destination.txt";
+var inputFileName = "file_source.json";
+var outputFileName = "file_destination.txt";
 
 // Let's run.
 processFile(inputFileName, outputFileName);
@@ -27,43 +29,65 @@ function processFile(inputFileName, outputFileName) {
         return;
     }
 
-    // Let's attempt to read source file.
+    // Attempt to read source file.
     fs.readFile(inputFileName, function readCallback(err, contents) {
 
-        // Error?
-        if(err){
-            console.log("Failed to read input file:" + err);
+        // Error reading?
+        if(err) {
+            console.log("Failed to read input file: " + err);
             return;
         }
 
-        // String contents
+        // Attempt to cast the contents into JSON. Check if parse fails?
         console.log("File Contents as String: " + contents);
-
-        // Attempt to cast the contents into JSON.
         var contentsObj = {};
         try {
             contentsObj = JSON.parse(contents);
             console.log("Parsed Contents: " + JSON.stringify(contentsObj));
         } catch (ex) {
-            // Parsing JSON String Failed.
             console.log("Source file is not in expected JSON format:" + ex);
             return;            
         }
 
-        // Expected Structure?
+        // Expected root element?
         if(!contentsObj.hasOwnProperty("students")) {
-            console.log("Expected students element not found");
+            console.log("Expected students element not found in: " + inputFileName);
             return;
         }
-        
-        // Iterate over every student and generate output string.
-        var outputString = "Id | First Name | Last Name | Score \n";
-        console.log("Students Found: " + contentsObj.students.length);
-        for(i = 0; i < contentsObj.students.length; i++) {
-            var student = contentsObj.students[i];
-            outputString += student.id + " | " + student.fName + " | " + student.lName + " | " + student.score + "\n";
+
+        // This must be an array.
+        if(!util.isArray(contentsObj.students)) {
+            console.log("Expected students array not found in: " + inputFileName);
+            return;
         }
 
+        // Student records found?
+        console.log("Students Found: " + contentsObj.students.length);
+        
+        //
+        // Iterate over every student and generate output string.
+        //
+        var studentsListString = "";
+        for(var i = 0; i < contentsObj.students.length; i++) {
+
+            // Student Object
+            var student = contentsObj.students[i];
+
+            // Validate all expected parameters present?
+            if(!student.hasOwnProperty("id") || !student.hasOwnProperty("fName") || !student.hasOwnProperty("lName") || !student.hasOwnProperty("score")) {
+                console.log("Skipping bad record: " + i);
+                continue;
+            }
+
+            // Construct record (string)
+            studentsListString += student.id + " | " + student.fName + " | " + student.lName + " | " + student.score + "\n";
+        }
+
+        //
+        // Generate final output file.
+        //
+        var outputString = "-----\nId | First Name | Last Name | Score\n-----\n";
+        outputString += studentsListString;
         console.log("Writing This Data: \n" + outputString);
         fs.writeFile(outputFileName, outputString, function writeCompletedCallback(err) {
 
